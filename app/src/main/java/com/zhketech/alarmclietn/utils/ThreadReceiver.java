8bedd5b8db649b7ad5ec4407e7068775ef14b1d9
wpfsean <wpfsean@126.com>
2018-04-10 14:45:38 +0800

    ThreadReceiver

---
+++ app/src/main/java/com/zhketech/alarmclietn/utils/ThreadReceiver.java
@@ -0,0 +1,31 @@
+package com.zhketech.alarmclietn.utils;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+/**
+ * Created by Root on 2018/4/10.
+ */
+
+public class ThreadReceiver extends BroadcastReceiver {
+
+    OnGetDateListern onGetDateListern = null;
+
+    public ThreadReceiver(OnGetDateListern onGetDateListern) {
+        this.onGetDateListern = onGetDateListern;
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+        if(onGetDateListern!=null){
+            onGetDateListern.getData("fgg");
+        }
+    }
+
+    public interface OnGetDateListern {
+        void getData(String str);
+    }
+    ;
+}
