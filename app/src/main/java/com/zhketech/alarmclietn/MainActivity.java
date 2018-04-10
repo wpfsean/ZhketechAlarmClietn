6c51d905b80e00d7c2ee15c764cc39ca5762e8fd
wpfsean <wpfsean@126.com>
2018-04-10 14:39:41 +0800

    不显示ip地址

--- app/src/main/java/com/zhketech/alarmclietn/MainActivity.java
+++ app/src/main/java/com/zhketech/alarmclietn/MainActivity.java
@@ -46,7 +46,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ip = getIPAddress(this);
-        Logutils.i("ip:" + ip);
+
     }
 
     @Override
